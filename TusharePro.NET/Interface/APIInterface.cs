7bab4796b5f22ce430bf6817bd2e2c288d1b1ab5
小野寺つさき <apertureelectronic@outlook.com>
2021-03-06 14:34:12 +0800

    Update license comment

--- TusharePro.NET/Interface/APIInterface.cs
+++ TusharePro.NET/Interface/APIInterface.cs
@@ -1,3 +1,17 @@
+// TusharePro.NET
+// Copyright(C) 2021 Aperture Electronic
+// API Copyright(C) Tushare which is following BSD-3-Clause License.
+
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or any later version.
+
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+
 using System;
 using System.Collections.Generic;
 using System.Reflection;
